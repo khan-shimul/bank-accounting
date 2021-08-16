352b40e384a113594d68c28fe57f6e985ee05ddc
shimulkhan <khanshimul196@gmail.com>
2021-08-16 16:46:29 +0600

    login page

---
+++ js/login.js
@@ -0,0 +1,11 @@
+document.getElementById('login-button').addEventListener('click', function () {
+    const emailInput = document.getElementById('email-input');
+    const userEmail = emailInput.value;
+    // get user pass
+    const passwordInput = document.getElementById('password-input');
+    const userPassword = passwordInput.value;
+
+    if (userEmail == 'sim@gmail.com' && userPassword == '123') {
+        window.location.href = 'account.html';
+    }
+});
